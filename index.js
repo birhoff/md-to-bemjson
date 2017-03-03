c91bcf2d3e5d50148a8f791730d87059f87b5f5c
Konstantin Gladkikh <birhoff@gmail.com>
2017-03-03 15:41:36 +0300

    Infrastructure

---
+++ index.js
@@ -0,0 +1,1 @@
+module.exports = require('./lib/md-2-bemjson');
---
+++ test/test.js
@@ -0,0 +1,7 @@
+const expect = require('chai').expect;
+
+describe('block', () => {
+    it('should 1 to be 1', () => {
+        expect(1).to.eql(1);
+    });
+});
