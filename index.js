f16c7b550159029efad6e6ee8c91ab054aae5dae
Konstantin Gladkikh <birhoff@gmail.com>
2017-03-13 20:00:55 +0300

    feat(core): convert md to bemjson
    Add partial convertion, only root ndoe

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
+'use strict';
+
 module.exports = require('./lib/md-2-bemjson');
--- test/test.js
+++ test/test.js
@@ -1,7 +1,22 @@
-const expect = require('chai').expect;
+'use strict';
 
-describe('block', () => {
-    it('should 1 to be 1', () => {
-        expect(1).to.eql(1);
+const fs = require('fs');
+
+const expect = require('chai').expect;
+const nodeEval = require('node-eval');
+
+const toBemjson = require('../index');
+const md = fs.readFileSync(__dirname + '/test-assets/test.md');
+const result = require('./test-assets/test.bemjson');
+
+describe('test', () => {
+    it('should convert md to bemjson', done => {
+        toBemjson(md)
+            .then(fileData => {
+                const bjson = nodeEval(fileData);
+                expect(bjson).to.deep.equal(result);
+            })
+            .catch(err => err)
+            .then(done);
     });
 });
